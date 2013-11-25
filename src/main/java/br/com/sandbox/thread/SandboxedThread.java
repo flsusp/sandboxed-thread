fa823d8bbc83bf59d4df48acaa783a9b64fd226d
Fabio Lima Santos <flsusp@gmail.com>
2013-11-24 22:39:00 -0200

    Sandboxing with SecurityManager

--- src/main/java/br/com/sandbox/thread/SandboxedThread.java
+++ src/main/java/br/com/sandbox/thread/SandboxedThread.java
@@ -1,14 +1,48 @@
 package br.com.sandbox.thread;
 
+import java.security.AccessControlException;
+import java.security.Permission;
+
 public class SandboxedThread<C> extends Thread {
 
 	private final SandboxedRunnable<C> runnable;
+	private boolean allowSettingSecurityManager;
+	private C context;
+	private SecurityManager oldSecurityManager;
 
 	public SandboxedThread(SandboxedRunnable<C> runnable) {
 		this.runnable = runnable;
 	}
 
 	public void start(C context) {
-		runnable.run(new SandboxedThreadInfo(Thread.currentThread().getName()), context);
+		this.context = context;
+		this.oldSecurityManager = System.getSecurityManager();
+
+		super.start();
+	}
+
+	public final void run() {
+		final SandboxedThreadInfo threadInfo = new SandboxedThreadInfo(Thread.currentThread().getName());
+
+		System.setSecurityManager(new SecurityManager() {
+
+			@Override
+			public void checkPermission(Permission perm) {
+				if (RuntimePermission.class.isInstance(perm)) {
+					if ("setSecurityManager".equals(perm.getName()) && allowSettingSecurityManager) {
+						return;
+					}
+				}
+				throw new AccessControlException("sandboxed access denied", perm);
+			}
+		});
+		allowSettingSecurityManager = false;
+		
+		try {
+			runnable.run(threadInfo, context);
+		} finally {
+			allowSettingSecurityManager = true;
+			System.setSecurityManager(oldSecurityManager);
+		}
 	}
 }
--- src/test/java/br/com/sandbox/thread/SandboxedThreadTest.java
+++ src/test/java/br/com/sandbox/thread/SandboxedThreadTest.java
@@ -1,13 +1,17 @@
 package br.com.sandbox.thread;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
 
 import org.junit.Test;
 
 public class SandboxedThreadTest {
 
 	@Test
-	public void testCreatingAndRunningValidCodeOnSandboxedThread() throws InterruptedException {
+	public void testRunningValidCodeOnSandboxedThread() throws InterruptedException {
 		SandboxedThread<TestContext> thread = new SandboxedThread<>(new SandboxedRunnable<TestContext>() {
 
 			@Override
@@ -22,6 +26,47 @@ public class SandboxedThreadTest {
 		thread.join();
 
 		assertTrue(context.wasExecuted());
+	}
+
+	@Test
+	public void testOpeningFileOnSandboxedThread() throws InterruptedException {
+		SandboxedThread<TestContext> thread = new SandboxedThread<>(new SandboxedRunnable<TestContext>() {
+
+			@Override
+			public void run(SandboxedThreadInfo info, TestContext context) {
+				try {
+					new File("/tmp/a").createNewFile();
+				} catch (IOException e) {
+				}
+				context.markAsExecuted();
+			}
+		});
+
+		TestContext context = new TestContext();
+
+		thread.start(context);
+		thread.join();
+
+		assertFalse(context.wasExecuted());
+	}
+
+	@Test
+	public void testOverritingSecurityManagerOnSandboxedThread() throws InterruptedException {
+		SandboxedThread<TestContext> thread = new SandboxedThread<>(new SandboxedRunnable<TestContext>() {
+
+			@Override
+			public void run(SandboxedThreadInfo info, TestContext context) {
+				System.setSecurityManager(null);
+				context.markAsExecuted();
+			}
+		});
+
+		TestContext context = new TestContext();
+
+		thread.start(context);
+		thread.join();
+
+		assertFalse(context.wasExecuted());
 	}
 }
 
